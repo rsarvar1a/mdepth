fix(decoder): most blatant off-by-one in history

mdepth/model/decoder.py
@@ -36,7 +36,7 @@ class DepthDecoder(nn.Module):
         conv1s = []
         conv2s = []
 
-        for i in range(self.length - 1):
+        for i in range(self.length):
             conv1s.append(
                 Block(
                     self.ichannels[-1]

script_pretrain.py
@@ -4,11 +4,11 @@ import torch
 
 from torch.utils.data import DataLoader
 
-from .mdepth.data import KittiDataset
-from .mdepth.model import MonocularDepth
-from .mdepth.train import train, display_loss_graph
-from .mdepth.transforms.preprocess import JointToTensor, JointRandomResizeCrop, JointNormalize
-from .mdepth.utils.loss import MonocularLoss
+from mdepth.data import KittiDataset
+from mdepth.model import MonocularDepth
+from mdepth.train import train, display_loss_graph
+from mdepth.transforms.preprocess import JointToTensor, JointRandomResizeCrop, JointNormalize
+from mdepth.utils.loss import MonocularLoss
 
 
 def main():
@@ -19,19 +19,19 @@ def main():
         JointNormalize([0.485, 0.456, 0.406], [0.225, 0.224, 0.225])
     ]
     
-    dataset = KittiDataset("data/Kitti2015", mode=train, transforms=transforms)
+    dataset = KittiDataset("data/Kitti2015", mode="train", transforms=transforms)
     dataloader = DataLoader(dataset, batch_size=10, shuffle=True)
     
     device = torch.device('cuda:0')
     net = MonocularDepth().to(device)
-    optimizer = torch.Adam(net.parameters(), lr=2e-5)
+    optimizer = torch.optim.Adam(net.parameters(), lr=2e-5)
     
     losses = train(
         net, 
         device=device, 
         dataloader=dataloader,
-        epochs=10,
-        loss=MonocularLoss(net.decoder.length),
+        epochs=1,
+        loss=MonocularLoss(),
         optimizer=optimizer
     )
     display_loss_graph(losses)