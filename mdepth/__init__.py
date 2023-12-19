0e90d7412897549960929a8c2042e8fb454d3cfd
Rohan Sarvaria <rsarvaria@uwaterloo.ca>
2023-12-18 19:25:20 -0500

    fix(test): imports for new functions

--- mdepth/__init__.py
+++ mdepth/__init__.py
@@ -1,6 +1,6 @@
 from . import transforms
 from .data import KittiDataset
 from .model import MonocularDepth
-from .test import test, show_results
+from .test import postprocess, test, show_results
 from .train import train, display_loss_graph
 from .utils.loss import MonocularLoss
